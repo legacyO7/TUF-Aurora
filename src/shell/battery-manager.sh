3ab50d4ba62439e6ee7a70d4afab28f03b19167f
legacyO7 <46623012+legacyO7@users.noreply.github.com>
2021-08-18 12:04:09 +0530

    enable battery manager on startuup

--- src/shell/battery-manager.sh
+++ src/shell/battery-manager.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-echo $1 > /sys/class/power_supply/BAT1/charge_control_end_threshold
+
+echo $1 | sudo tee /sys/class/power_supply/BAT1/charge_control_end_threshold > /dev/null
+cd /etc/systemd/system/
+sudo rm battery-manager.service
+echo "[Unit]
+Description=To set battery charge threshold
+After=multi-user.target
+StartLimitBurst=0
+
+[Service]
+Type=oneshot
+Restart=on-failure
+ExecStart=sudo /bin/bash -c 'echo $1 > /sys/class/power_supply/BAT1/charge_control_end_threshold'
+
+[Install]
+WantedBy=multi-user.target" > battery-manager.service
+
+sudo systemctl enable battery-manager.service
