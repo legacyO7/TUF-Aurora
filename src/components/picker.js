9e3640f358553a6b4b4fdceb6e521ee8b653c911
anit-sebastian <anit@geeks.ae>
2021-08-20 16:49:07 +0530

    remove components & set default color

--- src/components/picker.js
+++ src/components/picker.js
@@ -1,25 +1,60 @@
 const pickr = require('@simonwep/pickr');
+const paths = require('../path');
+const shell = require('shelljs');
+shell.config.execPath = shell.which('node').toString();
 
-module.exports = pickr.create({
-	el: '.color-picker',
-	theme: 'nano', 
-	padding: 8,
-	// or 'monolith', or 'nano'
-	swatches: [
-		'#ED1000',
-		'#FF0057',
-		'#2BFF01',
-		'#FF00E5',
-		'#005CFF',
-	],
-	components: {
-		hue: true,
-		preview: true,
-		interaction: {
-			hex: true,
-			input: true,
-			clear: true,
-			save: true,
-		},
-	},
+var debug = document.getElementById("debugger");
+var colorcode="#123456"
+async function getColor(path){
+	
+ return await new Promise((resolve, reject) => {	
+   shell.exec(`cat ${path}`, function(
+        error,
+        stdout,
+		stderr	
+    ) {
+	 resolve(stdout)	 
+    });
 });
+}
+ async function setPicker(){
+	  colorcode=('#'+await getColor(paths.path_red)+ await getColor(paths.path_green)+await getColor(paths.path_blue)).replace(/\s+/g, '')
+	 const picker= pickr.create({
+		el: '.color-picker',
+		theme: 'nano', 
+		padding: 8,
+		default: colorcode,
+		swatches: [
+			'#ED1000',
+			'#FF0057',
+			'#2BFF01',
+			'#FF00E5',
+			'#005CFF',
+		],
+		components: {
+			hue: true,
+			preview: true,
+			interaction: {
+				hex: true,
+				input: true,
+				clear: true,
+				save: true,
+			},
+		},
+	})
+	
+	picker.on('save', (color, instance) => {				
+	
+		try {
+			const splitHex = `${color.toHEXA()[0]} ${color.toHEXA()[1]} ${color.toHEXA()[2]}`;
+			shell.exec('bash ' + __dirname + `/../shell/color.sh ${splitHex}`);
+		
+		} catch (e) {
+			console.log('Error:', e.stack);
+		}
+	});	
+	
+}
+
+
+module.exports =setPicker();
