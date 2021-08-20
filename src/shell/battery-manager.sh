e8c75e32a2ab0611205f8e68ad1ebe15fad57e1f
anit-sebastian <anit@geeks.ae>
2021-08-21 01:16:47 +0530

    fa706: move battery-management apart from faustus module check

--- src/shell/battery-manager.sh
+++ src/shell/battery-manager.sh
@@ -2,7 +2,13 @@
 
 echo $1 | sudo tee /sys/class/power_supply/BAT1/charge_control_end_threshold > /dev/null
 cd /etc/systemd/system/
-sudo rm battery-manager.service
+sudo rm tuf-controller.service
+sudo systemctl disable tuf-controller.service
+if [ -d "/sys/devices/platform/faustus/" ]; then
+   execCommand='insmod /usr/bin/faustus.ko let_it_burn=1 && echo $1 > /sys/class/power_supply/BAT1/charge_control_end_threshold'
+else
+    execCommand='echo $1 > /sys/class/power_supply/BAT1/charge_control_end_threshold'
+fi
 echo "[Unit]
 Description=To set battery charge threshold
 After=multi-user.target
@@ -11,9 +17,9 @@ StartLimitBurst=0
 [Service]
 Type=oneshot
 Restart=on-failure
-ExecStart=sudo /bin/bash -c 'insmod /usr/bin/faustus.ko let_it_burn=1 && echo $1 > /sys/class/power_supply/BAT1/charge_control_end_threshold'
+ExecStart=sudo /bin/bash -c '$execCommand'
 
 [Install]
-WantedBy=multi-user.target" > battery-manager.service
+WantedBy=multi-user.target" > tuf-controller.service
 
-sudo systemctl enable battery-manager.service
+sudo systemctl enable tuf-controller.service
