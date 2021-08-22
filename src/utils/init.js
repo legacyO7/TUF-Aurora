dd7de173bb7d3e230ee08a935146ebc0d1e5683a
legacyO7 <46623012+legacyO7@users.noreply.github.com>
2021-08-22 13:49:34 +0530

    fa706: update appinfo

--- src/utils/init.js
+++ src/utils/init.js
@@ -4,21 +4,19 @@ const paths = require('../path');
 // check for faustus modules and load the configs on startup
 
 const initialize = () => {
-	if(fs.existsSync(`${paths.kModule}`)){
+    if (fs.existsSync(`${paths.kModule}`)) {
         document.getElementById('content').style.display = 'block'
         document.getElementById('blockuser').style.display = 'none'
-    }
-    else
-    {
+    } else {
         document.getElementById('content').style.display = 'none'
         document.getElementById('blockuser').style.display = 'block'
-    }  
-    
-    
-    document.getElementById(parseInt(shell.exec(`cat ${paths.brightness}`)),10).checked=true
-    document.getElementById(4+parseInt(shell.exec(`cat ${paths.speed}`)),10).checked=true
-    document.getElementById(7+parseInt(shell.exec(`cat ${paths.kblMode}`)),10).checked=true
-    
+    }
+
+
+    document.getElementById(parseInt(shell.exec(`cat ${paths.brightness}`)), 10).checked = true
+    document.getElementById(4 + parseInt(shell.exec(`cat ${paths.speed}`)), 10).checked = true
+    document.getElementById(7 + parseInt(shell.exec(`cat ${paths.kblMode}`)), 10).checked = true
+
 };
 
 module.exports = initialize;
