23e0e8e27c13540acde8262ef8cae466ab473db1
legacyO7 <46623012+legacyO7@users.noreply.github.com>
2021-09-23 13:08:42 +0530

    fa706: check for update and changelog

--- src/components/versionchecker.js
+++ src/components/versionchecker.js
@@ -2,84 +2,82 @@ const { ipcRenderer, dialog } = require('electron')
 var sudo = require('sudo-prompt');
 var { exec } = require('child_process');
 const { VTexec } = require('open-term')
+const { options, showdialog: ipcaction } = require('../global')
 const shell = require('shelljs');
 
-
-var options = {
-    name: 'Aurora',
-};
 
 async function getlatestvesrion(url) {
     let response = await fetch(url);
     let data = await response.json();
+    fetch("https://raw.githubusercontent.com/legacyO7/TUF-Aurora/beta/changelog.txt").then((r) => {
+        r.text().then((d) => {
+            document.getElementById('changelog').innerText = d
+        })
+    })
     return data.version;
 }
 
-async function execWaitForOutput(command, execOptions = {}) {
-    return new Promise((resolve, reject) => {
-        const childProcess = exec('git pull', execOptions);
-
-        // stream process output to console
-        childProcess.stderr.on('data', data => console.error(data));
-        childProcess.stdout.on('data', data => console.log(data));
-        // handle exit
-        childProcess.on('exit', () => resolve());
-        childProcess.on('close', () => resolve());
-        // handle errors
-        childProcess.on('error', error => reject(error));
-    })
-}
-
 const updatechecker = async() => {
-
     var currentversion, latestvesrion
-    ipcRenderer.send('appversion');
-    ipcRenderer.on('appversion-response', function(event, args) {
+
+    await ipcaction('appversion').then((args) => {
         currentversion = args[0]
-    });
+    })
 
-    var modal = document.getElementById("update-modal");
+    var uptext = document.getElementById("uptext")
+    uptext.innerText = "v" + currentversion
+    if (window.navigator.onLine) {
 
-    latestvesrion = (await getlatestvesrion("https://raw.githubusercontent.com/legacyO7/TUF-Aurora/master/package.json"))
+        var modal = document.getElementById("update-modal");
 
-    if (currentversion == latestvesrion)
-        console.log("everything up to date")
-    else {
-        modal.style.display = "block";
-        document.getElementById('update-text').innerText = `v${latestvesrion}`
+        latestvesrion = await getlatestvesrion("https://raw.githubusercontent.com/legacyO7/TUF-Aurora/master/package.json")
+
+        if (currentversion != latestvesrion)
+            console.log("everything up to date")
+        else {
+            modal.style.display = "block";
+            document.getElementById('update-text').innerText = `v${latestvesrion}`
+        }
+
+        shell.exec('rm -rf temp')
+
+        document.getElementById("btn-close").onclick = function() {
+            modal.style.display = "none";
+        }
+        document.getElementById("btn-update").onclick = function() {
+            doUpdate()
+        }
+
+
+        async function doUpdate() {
+            document.getElementById('appheader').style.marginLeft = "70px"
+            document.getElementById('upbar').style.visibility = "visible"
+            uptext.innerText = "Updating"
+            modal.style.display = "none";
+
+            shell.exec('rm -rf temp && mkdir temp && cd temp  && git clone https://github.com/legacyO7/TUF-Aurora.git')
+
+            const dialogoptions = {
+                type: 'question',
+                buttons: ['Yes, please'],
+                defaultId: 2,
+                title: 'Download Completed',
+                message: 'Download Completed',
+                detail: 'Close the app and update now',
+                checkboxChecked: false,
+            };
+
+            await ipcaction('showdialog', [dialogoptions]).then((args) => {
+                if (args[0].response == 0) {
+                    VTexec('bash TUF-Aurora/setup.sh')
+                    window.close()
+                }
+            })
+
+        }
+
     }
 
-    //shell.exec('rm -rf temp')
-
-    document.getElementById("btn-close").onclick = function() {
-        modal.style.display = "none";
-    }
-    document.getElementById("btn-update").onclick = function() {
-        modal.style.display = "none";
-        console.log('downloading sources')
-        shell.exec('rm -rf temp && mkdir temp && cd temp  && git clone https://github.com/legacyO7/TUF-Aurora.git')
-        console.log('downloading completed')
-
-
-        const dialogoptions = {
-            type: 'question',
-            buttons: ['Yes, please'],
-            defaultId: 2,
-            title: 'Download Completed',
-            message: 'Download Completed',
-            detail: 'Do you want to close the app and update now?',
-            checkboxChecked: false,
-        };
-
-
-        ipcRenderer.send('showdialog', [dialogoptions]);
-        ipcRenderer.on('dialog-response', function(event, args) {
-            if (args[0].response == 0) {
-                VTexec('bash TUF-Aurora/setup.sh')
-                window.close()
-            }
-        });
-    }
 
 }
 
