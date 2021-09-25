45bfde879a9f3c1967264064fb3a70780ede0052
legacyO7 <46623012+legacyO7@users.noreply.github.com>
2021-09-26 03:40:01 +0530

    fa 706: save changes to appdata

--- src/global.js
+++ src/global.js
@@ -1,4 +1,7 @@
-const { ipcRenderer } = require('electron')
+const { ipcRenderer } = require('electron');
+const shell = require('shelljs');
+const { existsSync } = require('fs');
+const untildify = require('untildify');
 
 var options = {
     name: 'Aurora',
@@ -6,10 +9,9 @@ var options = {
 
 var branch = "beta"
 
-var loc_aurora = "~/.tuf-aurora"
+var loc_aurora = untildify("~/.tuf-aurora");
 
 const ipcaction = async(name, options) => {
-    //  console.log(name)
     ipcRenderer.send(name, options);
     return await new Promise((resolve, reject) => {
         ipcRenderer.on(name + "-response", function(event, args) {
@@ -18,11 +20,30 @@ const ipcaction = async(name, options) => {
     });
 }
 
+async function fetchData(url, changelog) {
+    let response = await fetch(url);
+    let data = await response.json();
+    if (changelog)
+        getchangelog();
+    return data;
+}
+
 function getchangelog() {
     fetch("https://raw.githubusercontent.com/legacyO7/TUF-Aurora/" + branch + "/changelog.txt").then(async(r) => {
         document.getElementById('changelog').innerText = await r.text()
     })
 }
 
+async function saveDef(key, value) {
+    var defaults
+    if (existsSync(`${loc_aurora}/settings`)) {
+        defaults = await fetchData(`${loc_aurora}/settings`, false)
+    } else
+        defaults = { color: "#000000", k_effects: "0", k_speed: "0", k_brightness: "0" }
 
-module.exports = { options, ipcaction, branch, loc_aurora, getchangelog }
+    defaults[key] = value;
+    shell.exec("echo " + JSON.stringify(JSON.stringify(defaults)) + " > " + loc_aurora + "/settings")
+}
+
+
+module.exports = { options, ipcaction, branch, loc_aurora, getchangelog, saveDef, fetchData }
