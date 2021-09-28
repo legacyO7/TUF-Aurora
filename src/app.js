32fcb536bc9527de6e7e9babd7c926cd86f5206d
legacyO7 <46623012+legacyO7@users.noreply.github.com>
2021-09-28 13:58:07 +0530

    fa706: set color from config  on startup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const permsHandler = require('./utils/permshandler');
-const { initialize, setkeyboardsettings } = require('./utils/init');
+const { initialize } = require('./utils/init');
 const { setPicker } = require('./components/picker');
 const batterymanager = require('./components/batterymanager');
 const { updateCentre } = require('./components/updatecentre');
 const { keyboardSettings } = require('./components/keyboardSettings');
 
-permsHandler();
+await permsHandler();
 await initialize();
 updateCentre();
 setPicker();
