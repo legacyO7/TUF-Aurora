3001f30705a493478145452ce6eac5eca706e5c4
legacyO7 <46623012+legacyO7@users.noreply.github.com>
2021-11-18 13:51:32 +0530

    fa706: bring up aurora 2.0.0

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-const { permsHandler } = require('./utils/permshandler');
-const { initialize } = require('./utils/init');
-const { setPicker } = require('./components/picker');
-const batterymanager = require('./components/batterymanager');
-const { updateCentre } = require('./components/updatecentre');
-const { keyboardSettings } = require('./components/keyboardSettings');
+const { permsHandler } = require('../utils/permshandler');
+const { initialize } = require('../utils/init');
+const { setPicker } = require('../components/picker');
+const batterymanager = require('../components/batterymanager');
+const { updateCentre } = require('../components/updatecentre');
+const { keyboardSettings } = require('../components/keyboardSettings');
 
 await permsHandler();
 await initialize();
