689b0bc02c3a34a3dbcc29beb21246fba2236815
legacyO7 <46623012+legacyO7@users.noreply.github.com>
2021-11-26 19:54:40 +0530

    changes for appimage and snap

--- src/components/updatecentre.js
+++ src/components/updatecentre.js
@@ -1,4 +1,4 @@
-const { ipcaction, fetchData } = require('../global')
+const { ipcaction, fetchData, shelldir } = require('../global')
 const execShell = require('../pages/setup');
 
 async function updateCentre() {
@@ -17,31 +17,38 @@ async function updateCentre() {
 
     if (window.navigator.onLine) {
 
-        var modal = document.getElementById("update-modal");
-        let upbar = document.getElementById('upbar')
+        let dir = await shelldir()
 
-        latestversion = (await fetchData("https://raw.githubusercontent.com/legacyO7/TUF-Aurora/master/package.json", true)).version;
+        if (!dir.includes("/tmp/") && !dir.includes("/snap/")) {
+            var modal = document.getElementById("update-modal");
+            let upbar = document.getElementById('upbar')
 
-        if (currentversion != latestversion) {
-            modal.style.display = "block";
-            document.getElementById('update-text').innerText = `v${latestversion}`;
+            latestversion = (await fetchData("https://raw.githubusercontent.com/legacyO7/TUF-Aurora/master/package.json", true)).version;
+
+            if (currentversion != latestversion) {
+                modal.style.display = "block";
+                document.getElementById('update-text').innerText = `v${latestversion}`;
+            }
+
+            closebutton.onclick = function() {
+                modal.style.display = "none";
+            };
+            updatebutton.onclick = async function() {
+                document.getElementById('appheader').style.marginLeft = "70px";
+                upbar.style.visibility = "visible";
+                uptext.innerText = "Updating";
+                closebutton.style.display = "none"
+                updatebutton.style.display = "none"
+
+                await execShell("update").then(resp => {
+                    upbar.style.visibility = "hidden"
+                    uptext.innerText = "v" + currentversion;
+                });
+            };
+
         }
 
-        closebutton.onclick = function() {
-            modal.style.display = "none";
-        };
-        updatebutton.onclick = async function() {
-            document.getElementById('appheader').style.marginLeft = "70px";
-            upbar.style.visibility = "visible";
-            uptext.innerText = "Updating";
-            closebutton.style.display = "none"
-            updatebutton.style.display = "none"
 
-            await execShell("update").then(resp => {
-                upbar.style.visibility = "hidden"
-                uptext.innerText = "v" + currentversion;
-            });
-        };
 
 
 
